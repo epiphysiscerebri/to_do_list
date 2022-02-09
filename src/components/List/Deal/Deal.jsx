1c345015f5af1409efa0ced0eab5d17ec56f5bfa
epiphysis cerebri <zamurievanton@inbox.ru>
2022-02-09 17:25:30 +0300

    styling added

--- src/components/List/Deal/Deal.jsx
+++ src/components/List/Deal/Deal.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import classes from './Deal.module.css'
+import { useState } from 'react';
 
 
 function Deal (props) {
+  const  [classN, setClass] = useState(classes.deal);
 
+   
     
     let deleteDeal = () => {
      props.deleteDeal(props.id);
     }
 
+    let changeClick = () => {
+       if(classN === classes.deal) {
+           setClass(classes.active)
+       } else {
+           setClass(classes.deal)
+       }
+    }
 
     return (
-        <div className={classes.deal}>
+        <div className={classN}>
             <div className={classes.text}>{props.text}</div>
-            <input className={classes.checkbox} type='checkbox' />
+            <input className={classes.checkbox} type='checkbox' onChange= {changeClick}/>
             <button className={classes.delete} onClick={deleteDeal}/>
         </div>
     )
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,34 +1,54 @@
-import classes from './List.module.css'
-import Deal from './Deal/Deal';
-import React from 'react';
+import classes from "./List.module.css";
+import Deal from "./Deal/Deal";
+import React from "react";
 
-function List (props) {
+function List(props) {
+  let newDealElement = React.createRef();
 
-    
-    let newDealElement = React.createRef();
+  let dealElement = props.state.listComponent.map((deal) => (
+    <Deal
+      text={deal.text}
+      id={deal.id}
+      deleteDeal={props.state.deleteDeal}
+      changeComplete={props.changeComplete}
+      state={props.state}
+    />
+  ));
 
-    let dealElement = props.state.listComponent.map( deal => <Deal text= {deal.text} id= {deal.id} deleteDeal= {props.state.deleteDeal} changeComplete= {props.changeComplete} state= {props.state} />)
+  let addDeal = () => {
+    let text = newDealElement.current.value;
+    props.state.addDeal(text);
+  };
 
-    let addDeal = () => {
-        let text = newDealElement.current.value;
-        props.state.addDeal(text);
+  let updateNewTextDeal = () => {
+    let text = newDealElement.current.value;
+    props.state.updateNewTextDeal(text);
+  };
+
+  let changeKeyPress = (e) => {
+    if (e.key === "Enter") {
+      let text = newDealElement.current.value;
+      props.state.addDeal(text);
     }
+  };
 
-    let updateNewTextDeal = () => {
-        let text = newDealElement.current.value;
-        props.state.updateNewTextDeal(text); 
-    }
-
-
-    return (
-        <div className={classes.list}>
-            <div>
-            {dealElement}
-            <textarea className={classes.textArea} onChange={updateNewTextDeal} ref={newDealElement} value={props.state.newTextDeal}></textarea> 
-            <button className={classes.button} onClick={addDeal}>Add deal</button>
-            </div>
-        </div>
-    )
-};
+  return (
+    <div className={classes.list}>
+      <div>
+        <textarea
+          className={classes.textArea}
+          onChange={updateNewTextDeal}
+          ref={newDealElement}
+          value={props.state.newTextDeal}
+          placeholder="Введите значение..."
+        ></textarea>
+        <button className={classes.button} onKeyDown={changeKeyPress} onClick={addDeal}>
+          Add deal
+        </button>
+        {dealElement}
+      </div>
+    </div>
+  );
+}
 
 export default List;
